449079421c27ed83183c04a1435cc0d5cad4b6e9
Your Name <your_email@example.com>
2025-02-05 18:28:29 +0530

    seo is added

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://vitaboosthub.netlify.app',
+    generateRobotsTxt: true, // (optional) Generates a robots.txt file
+    // You can add more configuration options here
+  };
+  
